b14c5d479b1b07a089c74273e1c7514e39427b4b
Daniel Hu <hdmdhr@gmail.com>
2021-12-15 14:47:00 -0700

    Add use cases, add Encodable UpdatePasswordRequest

---
+++ FinalCodeChallenge/Services/Use Cases/AUpdateMyProfileUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  AUpdateMyProfileUseCase.swift
+//  FinalCodeChallenge
+//
+//  Created by Daniel Hu on 2021-12-15.
+//
+
+import Foundation
+
+protocol AUpdateMyProfileUseCase {
+    
+    func updateProfile(with newProfile: User, completion: (Result<User, Error>))
+    
+}
---
+++ FinalCodeChallenge/Services/Use Cases/AUpdatePasswordUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  AUpdatePasswordUseCase.swift
+//  FinalCodeChallenge
+//
+//  Created by Daniel Hu on 2021-12-15.
+//
+
+import Foundation
+
+protocol AUpdatePasswordUseCase {
+    
+    func updatePassword(with body: UpdatePasswordRequest, completion: (Result<Void, Error>))
+    
+}
